e78b956088e5ed22596f7196813c4cb41345b063
part16 <p.sixteenclub@gmail.com>
2022-11-01 07:01:44 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index,:show]
-  
+  before_action :contributor_confirmation, only: [:edit]
+  before_action :set_item, only: [:show,:edit]
+
+  def edit 
+  end
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -9,22 +14,7 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  def show
-    @item = Item.find(params[:id])
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-        redirect_to item_path(@item)
-      else
-        render :edit
-    end
+  def show  
   end
 
   def create
@@ -33,13 +23,31 @@ class ItemsController < ApplicationController
         redirect_to root_path
       else
         render :new, status: :unprocessable_entity
-    end
+      end
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+      if @item.save
+        redirect_to item_path(@item)
+      else
+        render :edit
+      end
+  end
 
   private
 
   def item_params
     params.require(:item).permit(:image,:item_name,:item_discribe,:category_id,:state_id,:postage_id,:scheduled_day_id,:ship_id,:item_price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == Item.find(params[:id]).user
+  end 
+
 end
