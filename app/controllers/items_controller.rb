30006ed3b1538cc483ba3cc7fa8b73df32e57e4d
part16 <p.sixteenclub@gmail.com>
2022-10-31 20:56:42 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,14 +13,26 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+        redirect_to item_path(@item)
+      else
+        render :edit
+    end
+  end
+
   def create
     @item = Item.new(item_params)
-    
-  
-    if @item.save
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
+      if @item.save
+        redirect_to root_path
+      else
+        render :new, status: :unprocessable_entity
     end
   end
 
