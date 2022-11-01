66cb4d2ccd29c5ac5144c067ddbfdf55dcb4aba4
part16 <p.sixteenclub@gmail.com>
2022-11-01 14:37:49 +0900

    商品削除機能の実装 3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index,:show]
-  before_action :contributor_confirmation, only: [:edit]
+  before_action :contributor_confirmation, only: [:edit,:update,:destroy]
   before_action :set_item, only: [:show,:edit,:update,:destroy]
 
   def edit 
